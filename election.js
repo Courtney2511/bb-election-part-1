90a0769be2c3104421b4290e39ca67c6f637be10
Courtney Noonan <courtneynoonan@me.com>
2016-09-05 14:49:06 -0400

    completing assignment

--- election.js
+++ election.js
@@ -1,10 +1,26 @@
-$(document).ready(function() {
+$(document).ready(function pollResults(){
 
   $.ajax({
     url: 'https://bb-election-api.herokuapp.com/',
     method: 'GET',
     data: { },
     dataType: 'JSON'
+  }).done(function(responseData){
+    var candidateJSON = responseData;
+    var candidateList = candidateJSON["candidates"];
+
+    $('#results').empty();
+
+    for(i=0; i<3; i++) {
+      var listItem = document.createElement('li');
+      var candidate = candidateList[i];
+      console.log(candidate);
+      $('#results').append(listItem);
+      var vote = $(listItem).html(candidate.name + ', ' + candidate.votes);
+    }
+
   });
+
+  $('button').on("click", pollResults);
 
 });
